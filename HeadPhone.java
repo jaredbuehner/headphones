1296d4dea838747af32ae5d2214cb5dd8c2f28b7
Jared Buehner <56742442+jaredbuehner@users.noreply.github.com>
2020-04-11 23:39:43 -0400

    Add files via upload

---
+++ HeadPhone.java
@@ -0,0 +1,119 @@
+/*
+* File: HeadPhone.java
+* Author: Jared Buehner
+* Date: February 22, 2020
+* Purpose: This program creates constructors for HeadPhone objects to show the specifications
+* of headphones. It is capabale of changing the headphone volume as well.
+*/
+// Imports.
+import java.awt.Color;
+import java.util.Scanner;
+
+public class HeadPhone {
+    // Private and constant data fields.
+    final int LOW = 1;
+    final int MEDIUM = 2;
+    final int HIGH = 3;
+    private int volume;
+    private boolean pluggedIn;
+    private String manufacturer;
+    private Color headPhoneColor;
+    private String headPhoneModel;
+
+    // Constructor.
+    public HeadPhone (int sVolume, boolean sPluggedIn, String sManufacturer, Color sHeadPhoneColor, String sHeadPhoneModel) {
+        volume = sVolume;
+        pluggedIn = sPluggedIn;
+        manufacturer = sManufacturer;
+        headPhoneColor = sHeadPhoneColor;
+        headPhoneModel = sHeadPhoneModel;
+    }
+    // Default constructor.
+    public HeadPhone() {
+        volume = MEDIUM;
+        pluggedIn = false;
+        manufacturer = "Audio-Technica";
+        headPhoneColor = (Color.black);
+        headPhoneModel = "ATH-M50X";
+    }
+    // Setter methods.
+    public void setVolume(int sVolume) {
+        sVolume = volume;
+    }
+    public void setPluggedIn(boolean sPluggedIn) {
+        sPluggedIn = pluggedIn;
+    }
+    public void setManufacturer(String sManufacturer) {
+        sManufacturer = manufacturer;
+    }
+    public void setHeadPhoneColor(Color sHeadPhoneColor) {
+        sHeadPhoneColor = headPhoneColor;
+    }
+    public void setHeadPhoneModel(String sHeadPhoneModel) {
+        sHeadPhoneModel = headPhoneModel;
+    }
+    // Getter methods.
+    public int getVolume() {
+        return volume;
+    }
+    public boolean getPluggedIn() {
+        return pluggedIn;
+    }
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public Color getHeadPhoneColor() {
+        return headPhoneColor;
+    }
+    public String getHeadPhoneModel() {
+        return headPhoneModel;
+    }
+    // Overrides toString() methods for the HeadPhone objects.
+    public String toString() {
+
+        String colorName = "Unknown";
+        // Converts Color RGB values into a String containing the color name.
+        if (headPhoneColor.equals(Color.red)) {
+        colorName = "Red";
+        }
+        else if (headPhoneColor.equals(Color.blue)) {
+        colorName = "Blue";
+        }
+        else if (headPhoneColor.equals(Color.black)) {
+        colorName = "Black";
+        }
+        else if (headPhoneColor.equals(Color.white)) {
+        colorName = "White";
+        }
+        else if (headPhoneColor.equals(Color.green)) {
+        colorName = "Green";
+        }
+        // Returned string.
+        return ("Volume Is: " + volume + "\n" + "Are Headphones Plugged In?:"
+                + pluggedIn + "\n" + "Manufacturer is: " + manufacturer + "\n"
+                + "Headphone Color Is: " + colorName + "\n"
+                + "Headphone Model Is: " + headPhoneModel);
+    }
+    // Method that allows the user to change the volume of their headphones.
+    public String changeVolume() {
+
+        String currentVolume = "Unknown";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Set volume to 1, 2, or 3: ");
+        int volume = sc.nextInt();
+        // Converts the values of the volume variable to a string.
+        if (volume == LOW) {
+            currentVolume = "Low";
+            System.out.println("The volume has been set to: ");
+        }
+        if (volume == MEDIUM) {
+            currentVolume = "Medium";
+            System.out.println("The volume has been set to: ");
+        }
+        if (volume == HIGH) {
+            currentVolume = "High";
+            System.out.println("The volume has been set to: ");
+        }
+        return currentVolume;
+    }
+}
---
+++ TestHeadPhone.java
@@ -0,0 +1,49 @@
+/*
+* File: HeadPhone.java
+* Author: Jared Buehner
+* Date: February 22, 2020
+* Purpose: This program calls methods from HeadPhone.java and creates three headphones and displays
+* their specifications to the standard output and allows for the ability to change the headphone volume.
+*/
+// Imports.
+import java.awt.Color;
+
+public class TestHeadPhone {
+    public static void main(String[] args) {
+        // Creates three new headphones.
+        HeadPhone audioTechnica = new HeadPhone();
+        HeadPhone bose = new HeadPhone(3, true, "Bose", Color.blue, "QC-35");
+        HeadPhone sennheiser = new HeadPhone(1, true, "Sennheiser", Color.red, "HMEC46");
+        // Calling getters and assigning those values to variables.
+        int theVolume = audioTechnica.getVolume();
+        boolean thePluggedIn = audioTechnica.getPluggedIn();
+        String theManufacturer = audioTechnica.getManufacturer();
+        Color theColor = audioTechnica.getHeadPhoneColor();
+        String theModel = audioTechnica.getHeadPhoneModel();
+        // Setters for first object.
+        audioTechnica.setVolume(theVolume);
+        audioTechnica.setPluggedIn(thePluggedIn);
+        audioTechnica.setManufacturer(theManufacturer);
+        audioTechnica.setHeadPhoneColor(theColor);
+        audioTechnica.setHeadPhoneModel(theModel);
+        // Setters for second object.
+        bose.setVolume(theVolume);
+        bose.setPluggedIn(thePluggedIn);
+        bose.setManufacturer(theManufacturer);
+        bose.setHeadPhoneColor(theColor);
+        bose.setHeadPhoneModel(theModel);
+        // Setters for third object.
+        sennheiser.setVolume(theVolume);
+        sennheiser.setPluggedIn(thePluggedIn);
+        sennheiser.setManufacturer(theManufacturer);
+        sennheiser.setHeadPhoneColor(theColor);
+        sennheiser.setHeadPhoneModel(theModel);
+        // Standard output.
+        System.out.println(audioTechnica);
+        System.out.println(audioTechnica.changeVolume());
+        System.out.println(bose);
+        System.out.println(bose.changeVolume());
+        System.out.println(sennheiser);
+        System.out.println(sennheiser.changeVolume());
+    }
+}
